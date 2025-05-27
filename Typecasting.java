564b20528e74da814099346615b658a3d293aeb6
Rankit945 <rankitmittal07@gmail.com>
2025-05-27 16:09:43 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Typecasting{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+       //float num = input.nextInt();
+       // System.out.println(num);
+
+        int num2 = (int)(65.67f);
+        System.out.println(num2); 
+    }
+}
